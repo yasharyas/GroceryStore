3c95d05f9c497a466a2b40451e879734c6c70161
yasharyas <yasharyas@proton.me>
2024-04-20 22:31:42 +0530

    established sql connection & created function to read from sql

---
+++ backend/sql_connection.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+__cnx = None
+
+def get_sql_connection():
+    global __cnx
+    if __cnx is None:
+        cnx = mysql.connector.connect(user='root', password='Y@sharya2004',
+                                    host='127.0.0.1',
+                                    database='gs')
+    return cnx
