cd3c53ead349d5b7024d9944114c73db74544468
yasharyas <yasharyas@proton.me>
2024-04-20 22:48:27 +0530

    added insertion and deletion function

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -25,6 +25,29 @@ def get_all_products(connection):
 
     return response
 
-if __name__ == '__main__':
-    connection = get_sql_connection()
-    print(get_all_products(connection))
+def insert_new_product(connection,product):
+    cursor =connection.cursor()
+    query = ("insert into products "
+            "(name, uom_id,price_per_unit) "
+            "values(%s,%s,%s)"
+    )
+    data = (product['product_name'],product['uom_id'],product['price_per_unit'])
+    cursor.execute(query,data)
+    connection.commit()
+    return cursor.lastrowid
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products where product_id=" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
+
+    return cursor.lastrowid
+
+# if __name__ == '__main__':
+#     connection = get_sql_connection()
+#     print(insert_new_product(connection,{
+#         'product_name':'brinjal',
+#         'uom_id':'1',
+#         'price_per_unit':22
+#     }))
