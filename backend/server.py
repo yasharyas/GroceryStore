b3e8c332ad94f80b066ff97ea915fd5695277fcc
yasharyas <yasharyas@proton.me>
2024-04-23 01:41:24 +0530

    added fucntion to get products

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, jsonify
+import products_dao
+from sql_connection import get_sql_connection
 
 app=Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "flask test"
+connection=get_sql_connection()
 
+@app.route('/getProducts',methods=['GET'])
+def get_products():
+    products = products_dao.get_all_products(connection)
+    response = jsonify(products) #converts into json [flask displays only json]
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
 if __name__ =="__main__":
     print("Starting python flask server for grocery store management system!")
